63d3665ab2a5d83ae2dbbcf5e15c5175a6298864
Ivan Miletich <john.weaver.dev@gmail.com>
2019-10-25 00:16:05 +0300

    import defaults to Main module

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color 
+
+import Game 
+import Rendering
+import Logic
+
+window = InWindow "TicTac" (640, 480) (100, 100)
+
+backgroundColor = makeColor 0 0 0 255
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = play backgroundColor 30  initialGame gameAsPicture transformGame (const id) 
